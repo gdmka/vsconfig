26e5d657fb4f854241e9b7924f2c2e6bda12d822
Alexey Haidamaka <gdmka@icloud.com>
2020-04-01 19:19:51 +0300

    get extension list asking vscode directly

--- dump_ext_mac.sh
+++ dump_ext_mac.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 #disabling noclobber since i don't care to diff extension versions line by line over time - reflog will remember all changes
-ls -l ~/.vscode/extensions/ | awk '{print $9}'| sort >| extensions && echo $(date) >> extensions
+code --list-extensions --show-versions >| extensions && echo "$(date)" >> extensions
